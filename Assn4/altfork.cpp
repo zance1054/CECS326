13a23c4d5ba4124efe26035b950a9eee56f13f3c
alec_fielding <alecfielding95@gmail.com>
2018-12-06 11:45:52 -0800

    bug fixes

--- Assn4/altfork.cpp
+++ Assn4/altfork.cpp
@@ -19,29 +19,12 @@ int main() {
 
 	int count;
 	long childPID;
-	bool continue = true;
+	bool cont= true;
 	string target;
 	string replacement;
-	string text = """A SCANDAL IN BOHEMIA Arthur Conan Doyle of contents Chapter 1 Chapter 2 Chapter 3 CHAPTER I To Sherlock Holmes she is always the woman. I have seldom heard him
-       mention her under any other name. In his eyes she eclipses and
-       predominates the whole of her sex. It was not that he felt any
-       emotion akin to love for Irene Adler. All emotions, and that one
-       particularly, were abhorrent to his cold, precise but admirably
-       balanced mind. He was, I take it, the most perfect reasoning and
-       observing machine that the world has seen, but as a lover he would
-       have placed himself in a false position. He never spoke of the softer
-       passions, save with a gibe and a sneer. They were admirable things
-       for the observer--excellent for drawing the veil from men's motives
-       and actions. But for the trained reasoner to admit such intrusions
-       into his own delicate and finely adjusted temperament was to
-       introduce a distracting factor which might throw a doubt upon all his
-       mental results. Grit in a sensitive instrument, or a crack in one of
-       his own high-power lenses, would not be more disturbing than a strong
-       emotion in a nature such as his. And yet there was but one woman to
-       him, and that woman was the late Irene Adler, of dubious and
-       questionable memory."""
+	string text = " A SCANDAL IN BOHEMIA Arthur Conan Doyle of contents Chapter 1 Chapter 2 Chapter 3 CHAPTER I To Sherlock Holmes she is always the woman. I have seldom heard him mention her under any other name. In his eyes she eclipses and predominates the whole of her sex. It was not that he felt any emotion akin to love for Irene Adler. All emotions, and that one particularly, were abhorrent to his cold, precise but admirably balanced mind. He was, I take it, the most perfect reasoning and observing machine that the world has seen, but as a lover he would have placed himself in a false position. He never spoke of the softer passions, save with a gibe and a sneer. They were admirable things for the observer--excellent for drawing the veil from men's motives and actions. But for the trained reasoner to admit such intrusions into his own delicate and finely adjusted temperament was to introduce a distracting factor which might throw a doubt upon all his mental results. Grit in a sensitive instrument, or a crack in one of his own high-power lenses, would not be more disturbing than a strong emotion in a nature such as his. And yet there was but one woman to him, and that woman was the late Irene Adler, of dubious and questionable memory.";
 
-	while (continue)
+	while (cont)
 	{
 		sleep(1);
 		cout << "Enter a target string that will be replaced within the text document or :wq to exit the program" << endl;
@@ -69,7 +52,7 @@ cout << "Enter a replacement string that will replaced the target string in the 
 					if (pos == -1)
 					{
 						cout << "." << endl;
-						sleep(.1);
+						sleep(1);
 						initial = true;
 					}
 					else
